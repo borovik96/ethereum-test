26bd6b09d8ed0ce573faa80f01b44fb208106b18
mixzenin <mix.zenin.ifmo@yandex.ru>
2017-11-19 01:26:04 +0300

    voucher
    v 2.0.3

--- voucher.sol
+++ voucher.sol
@@ -5,6 +5,7 @@ contract Voucher {
   struct Ticket {
     bytes16 serialNumber;
     bytes16 warrantyCase;
+    bytes16 productName;
     uint fn;
     uint fd;
     uint fpd;
@@ -25,7 +26,8 @@ contract Voucher {
     return buyer.amountOfTickets;
   }
 
-  function getTickets(uint cardNumber, uint idTicket) public constant returns(
+  function getTicket(uint cardNumber, uint idTicket) public constant returns(
+                                                            bytes16 _productName,
                                                             bytes16 _serialNumber,
                                                             uint _fn,
                                                             uint _fd,
@@ -34,15 +36,16 @@ contract Voucher {
                                                             bytes16 _warrantyCase
                                                             ) {
     Buyer storage buyer = listOfBuyers[cardNumber];
-    return (buyer.listOfTickets[idTicket].serialNumber,
-            buyer.listOfTickets[idTicket].fn,
-            buyer.listOfTickets[idTicket].fd,
-            buyer.listOfTickets[idTicket].fpd,
-            buyer.listOfTickets[idTicket].guaranteeTime,
-            buyer.listOfTickets[idTicket].warrantyCase);
+    _productName = buyer.listOfTickets[idTicket].productName;
+    _serialNumber = buyer.listOfTickets[idTicket].serialNumber;
+    _fn = buyer.listOfTickets[idTicket].fn;
+    _fd = buyer.listOfTickets[idTicket].fd;
+    _fpd = buyer.listOfTickets[idTicket].fpd;
+    _guaranteeTime = buyer.listOfTickets[idTicket].guaranteeTime;
+    _warrantyCase = buyer.listOfTickets[idTicket].warrantyCase;
   }
 
-  function setTicket(uint cardNumber, uint fn, uint fd, uint fpd, bytes16 serialNumber, uint guaranteeTime, bytes16 warrantyCase) public returns (uint idTicket){
+  function setTicket(uint cardNumber, uint fn, uint fd, uint fpd, bytes16 serialNumber, uint guaranteeTime, bytes16 warrantyCase, bytes16 productName) public returns (uint idTicket){
     Buyer storage buyer = listOfBuyers[cardNumber];
     buyer.amountOfTickets++;
     Ticket storage ticket = buyer.listOfTickets[buyer.amountOfTickets];
@@ -52,6 +55,7 @@ contract Voucher {
     ticket.fpd = fpd;
     ticket.guaranteeTime = guaranteeTime;
     ticket.warrantyCase = warrantyCase;
+    ticket.productName = productName;
     return buyer.amountOfTickets;
   }
 
