f334baac7d152b729006e15e30731a3e1a3cb310
Vladimir Borovik <bobahbdb@gmail.com>
2017-11-19 02:09:32 +0300

    senya Pidor slomal vse

--- node-app/utils.js
+++ node-app/utils.js
@@ -5,7 +5,7 @@ const utils = {
 
   calculateGuaranteeTime(timeOfBuying, during) {
     const date = new Date(timeOfBuying);
-    return date.setMonth(d.getMonth() + during);
+    return date.setMonth(date.getMonth() + during);
   }
 }
 
