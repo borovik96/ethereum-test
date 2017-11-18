53ee2401bb39bc242a11162a4ea94d2624af9643
mixzenin <mix.zenin.ifmo@yandex.ru>
2017-11-18 18:18:36 +0300

    voucher
    v 1.0.5

--- voucher.sol
+++ voucher.sol
@@ -4,8 +4,10 @@ contract Voucher {
   struct Ticket {
     bytes16 serialNumber;
     uint idTicket;
-    uint8 numberVoucher;
-    uint16 time;
+    uint16 fn;
+    uint16 fd;
+    uint16 fpd;
+    uint16 guaranteeTime;
   }
   mapping (uint => Ticket) listOfTickets;
   uint idTickets;
@@ -15,13 +17,20 @@ contract Voucher {
     return (listOfTickets[idTicket]);
   }
 
-  function setTicket(uint8 numberVoucher, bytes16 serialNumber, uint16 time) public returns (uint){
+  function setTicket(uint16 fn, uint16 fd, uint16 fpd, bytes16 serialNumber, uint16 guaranteeTime) public returns (uint){
     idTickets++;
     listOfTickets[idTickets].serialNumber = serialNumber;
-    listOfTickets[idTickets].numberVoucher = numberVoucher;
-    listOfTickets[idTickets].time = time;
+    listOfTickets[idTickets].fn = fn;
+    listOfTickets[idTickets].fd = fd;
+    listOfTickets[idTickets].fpd = fpd;
+    listOfTickets[idTickets].guaranteeTime = guaranteeTime;
     setTicketEvent(msg.sender, idTickets);
     return idTickets;
+  }
+
+  function checkGuarantee(uint idTicket, uint16 time) public constant returns(uint) {
+    require(time <= listOfTickets[idTicket].guaranteeTime);
+    return listOfTickets[idTicket].guaranteeTime - time;
   }
 
 }
